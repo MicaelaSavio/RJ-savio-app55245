44f7f2af89e3169dbad893932b4f17172004f6dd
MicaelaSavio <micaela.savio@hotmail.com>
2023-07-04 15:41:44 -0300

    cambios realizados en rama de prueba

--- rjs-savio-proyect/src/App.jsx
+++ rjs-savio-proyect/src/App.jsx
@@ -1,19 +1,17 @@
-import { Header } from "./components/Header/Header.jsx"
-import './App.css'
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './components/NavBar/NavBar.jsx';
-
+import { Header } from "./components/Header/Header.jsx";
+import "./App.css";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  
   return (
-<div>
-  <Header/>
-  <NavBar/>
-  <ItemListContainer greeting="¡Hola, bienvenid@ al catálogo!" />
-</div>
-  )
+    <div>
+      <Header />
+      <NavBar />
+      <ItemListContainer greeting='¡Hola, bienvenid@ al catálogo!' />
+    </div>
+  );
 }
 
-export default App
+export default App;
