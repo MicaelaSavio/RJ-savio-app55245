372b25676ec466763f687b09b1b6f33b200517b1
MicaelaSavio <micaela.savio@hotmail.com>
2023-08-28 16:16:44 -0300

    se trabajo en checkout y carrito de compras, se evita enviar formulario vacio

--- rjs-savio-proyect/src/components/Checkout/Checkout.jsx
+++ rjs-savio-proyect/src/components/Checkout/Checkout.jsx
@@ -17,47 +17,95 @@ const Checkout = () => {
         email: ''
     })
 
+    const [validations, setValidations] = useState({
+        nombre: true,
+        direccion: true,
+        email: true
+    });
+
     const handleInputChange = (e) => {
+        const { name, value } = e.target;
 
         setValues({
             ...values,
-            [e.target.name]: e.target.value
+            [name]: value
         })
 
+        setValidations({
+            ...validations,
+            [name]: true
+        });
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
-        // Verifica si algun campo está vacío
-        if (!values.nombre || !values.direccion || !values.email) {
+
+        let allValid = true;
+
+
+        // Validar campos requeridos
+
+
+        if (!values.nombre) {
+            setValidations({
+                ...validations,
+                nombre: false
+            });
+            allValid = false;
+        }
+
+        if (!values.direccion) {
+            setValidations({
+                ...validations,
+                direccion: false
+            });
+            allValid = false;
+
+
+        }
+
+
+        // Validar formato de email
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(values.email)) {
+            setValidations({
+                ...validations,
+                email: false
+            });
+            allValid = false;
+        }
+
+        if (!allValid) {
+
+
             Swal.fire({
                 icon: 'error',
                 title: 'Campos Incompletos',
                 text: 'Por favor, completa todos los campos antes de enviar el formulario.'
             });
-            return; 
+            return;
         }
-    
+
         const orden = {
             cliente: values,
             item: carrito[0].map(item => ({ id: item.id, nombre: item.nombre, precio: item.precio })),
             total: getTotal(),
             fyh: new Date()
         };
-    
+
         try {
             const orderRef = collection(db, "orders");
             const docRef = await addDoc(orderRef, orden);
-    
+
             console.log(docRef.id);
             vaciarCarrito();
             setOrderId(docRef.id);
         } catch (error) {
             console.error("Error al enviar la orden:", error);
         }
+
+
     }
-    
 
     if (orderId) {
         return (
@@ -70,18 +118,16 @@ const Checkout = () => {
                 <br />
                 <br />
                 <br />
-                
+
                 <h2>Tu orden ha sido enviada!</h2>
                 <hr />
-               
+
                 <p className="order">Tu numero de pedido es: <strong>{orderId}</strong></p>
 
                 <Link to="/"><button className="btnchkt">Volver a inicio</button></Link>
             </div>
         )
     }
-
-    
 
     return (
         <div className="container">
@@ -99,7 +145,6 @@ const Checkout = () => {
             <form onSubmit={handleSubmit}>
 
                 <input
-
                     onChange={handleInputChange}
                     value={values.nombre}
                     type="text"
@@ -107,18 +152,19 @@ const Checkout = () => {
                     placeholder="Ingresa tu nombre"
                     name="nombre"
                 />
-                <input
+                {!validations.nombre && <p className="error-message">El nombre es requerido</p>}
 
+                <input
                     onChange={handleInputChange}
                     value={values.direccion}
                     type="text"
                     className=""
                     placeholder="Ingresa tu direccion"
                     name="direccion"
-    
                 />
-                <input
+                {!validations.direccion && <p className="error-message">La dirección es requerida</p>}
 
+                <input
                     onChange={handleInputChange}
                     value={values.email}
                     type="email"
@@ -126,23 +172,20 @@ const Checkout = () => {
                     placeholder="Ingresa tu email"
                     name="email"
                 />
+                {!validations.email && <p className="error-message">Ingresa un email válido</p>}
 
-                <button className="btnchkt" onClick={vaciarCarrito}>Enviar</button>
-      
-                
-                
-                
+                <button
+                    className="btnchkt"
 
+                    disabled={!validations.nombre || !validations.direccion || !validations.email}
+                >
+                    Enviar
+                </button>
             </form>
-            
+
         </div>
 
-        
     )
-
-    
-    
 }
 
-
-export default Checkout
+export default Checkout;
